705443645209f26b852fe8d6b1825d6b31ebd0b8
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2020-05-19 13:22:08 -0300

    add logic with *ngIf directive

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,4 +6,26 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
     pageTitle: string = 'Product list';
+    products: any[] = [
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2019",
+            "description": "Descricao",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "assets/images/garden_cart.png"
+        },
+        {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX 9238",
+            "releaseDate": "April 02, 2020",
+            "description": "Descricao Hammer",
+            "price": 8.99,
+            "starRating": 4.8,
+            "imageUrl": "assets/images/hammer.png"
+        }
+    ];
 }
